8efaa9b506c529db0cd042f256632b5537617151
LunGomes <luanagomessilva112806@gmail.com>
2024-08-19 20:37:17 -0300

    Add pesquisar funcional

--- src/componentes/Pesquisa/Pesquisa.jsx
+++ src/componentes/Pesquisa/Pesquisa.jsx
@@ -1,6 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 function Pesquisa() {
+    const location = useLocation();
+
+    if (location.pathname === "/") return null;
+
     return ( 
-        
         <>
         <input className= "border-transparent px-6 m-6 p-2 bg-clarinho rounded-full border-3 placeholder-white" type="text" id="buscar" placeholder='Digite sua busca'/>
         <hr className="h-px m-2 border-0 bg-roxinho p-0"></hr>
