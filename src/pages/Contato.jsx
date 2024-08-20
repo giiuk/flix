5d7d22f07ae35a6c5d4c2948f4d0f03a3e4a8aec
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-20 08:59:10 -0300

    Fim não

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -5,7 +5,7 @@ function Contato() {
     return ( 
         <>
          
-        <div className="ms-9 mr-9 p-10 mt-9 resize-none rounded-xl bg-clarinho border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="ms-9 mr-9 p-10 mb-9 mt-9 resize-none rounded-xl bg-clarinho border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700">
         <h5 className="mb-4 text-2xl mt-0 font-bold tracking-tight text-roxinho dark:text-white">Contato</h5>
       
         <h1 className="font-normal text-lg text-black dark:text-gray-400">Gioboxd</h1>
